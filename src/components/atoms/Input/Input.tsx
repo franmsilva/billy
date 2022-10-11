32c5ee3e34470aad741ff819b58ab0f865c4903b
Francisco Silva <fmsilva1996@gmail.com>
2022-10-11 08:59:59 +0100

    feat: make Input atom a controlled field

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,18 +1,18 @@
-import { FC, HTMLInputTypeAttribute } from 'react';
+import { ChangeEventHandler, FC } from 'react';
 
 import { ETypography } from '@src/types/typography';
 
 import * as S from './Input.styled';
 
-interface InputProps {
+interface InputProps extends Partial<HTMLInputElement> {
   id: string;
-  value?: string;
+  name: string;
+  value: string;
   label?: string;
-  type?: HTMLInputTypeAttribute;
-  placeholder?: string;
+  onChange: ChangeEventHandler;
 }
 
-const Input: FC<InputProps> = ({ id, value, label = '', type = 'text', placeholder = '' }) => {
+const Input: FC<InputProps> = ({ id, name, value, label, type, placeholder, onChange }) => {
   return (
     <S.Wrapper>
       {label && (
@@ -20,7 +20,14 @@ const Input: FC<InputProps> = ({ id, value, label = '', type = 'text', placehold
           {label}
         </S.Label>
       )}
-      <S.Input id={id} type={type} value={value} placeholder={placeholder}></S.Input>
+      <S.Input
+        id={id}
+        name={name}
+        type={type || 'text'}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
     </S.Wrapper>
   );
 };
