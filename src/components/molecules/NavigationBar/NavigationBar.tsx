2f07c1fd36b739ade687b45b71417be7c22e01ba
jkellerman <89940123+jkellerman@users.noreply.github.com>
2023-01-18 16:50:44 +0000

    chore: edit logo names

--- src/components/molecules/NavigationBar/NavigationBar.tsx
+++ src/components/molecules/NavigationBar/NavigationBar.tsx
@@ -6,7 +6,7 @@ import * as S from './NavigationBar.styled';
 const NavigationBar: FC = () => (
   <S.Header>
     <S.LogoContainer>
-      <Image alt="Logo" src="/logo.svg" width={40} height={40} />
+      <Image alt="Logo" src="/logo-light.svg" width={40} height={40} />
     </S.LogoContainer>
     <S.UserContainer>
       <S.Image alt="Profile" src="/image-avatar.jpg" width={40} height={40} />
