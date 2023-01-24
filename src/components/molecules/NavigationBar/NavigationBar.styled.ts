cce346996a50b42b61432cb7b09aa4c3524b6d36
Francisco Silva <fmsilva1996@gmail.com>
2023-01-24 15:12:03 +0200

    feat: add responsive styles to NavigationBar

--- src/components/molecules/NavigationBar/NavigationBar.styled.ts
+++ src/components/molecules/NavigationBar/NavigationBar.styled.ts
@@ -1,48 +1,105 @@
 import NextImage from 'next/image';
 import styled, { css } from 'styled-components';
 
+import { devices } from '@styles/mediaQueries';
+import { flexCenter } from '@styles/snippets';
+
+const MOBILE_HEIGHT = 72;
+const TABLET_HEIGHT = 80;
+const LAPTOP_WIDTH = 100;
+
 export const Header = styled.header`
-  height: 100vh;
-  width: 100px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background-color: ${({ theme }) => theme.colors.slateGray};
-  border-top-right-radius: ${({ theme }) => theme.borderRadius[20]};
-  border-bottom-right-radius: ${({ theme }) => theme.borderRadius[20]};
-  overflow: hidden;
   display: flex;
-  flex-direction: column;
   justify-content: space-between;
+  width: 100%;
+  height: ${MOBILE_HEIGHT}px;
   z-index: ${({ theme }) => theme.zIndex.navbar};
+  background-color: ${({ theme }) => theme.colors.slateGray};
+
+  @media ${devices.tablet} {
+    height: ${TABLET_HEIGHT}px;
+  }
+
+  @media ${devices.laptop} {
+    height: 100vh;
+    width: ${LAPTOP_WIDTH}px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    border-top-right-radius: ${({ theme }) => theme.borderRadius[20]};
+    border-bottom-right-radius: ${({ theme }) => theme.borderRadius[20]};
+    overflow: hidden;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 `;
 
 export const LogoContainer = styled.div`
   ${({ theme }) => css`
-    height: 100px;
-    width: 100px;
-    background: linear-gradient(
-      to bottom,
-      ${theme.colors.cornflowerBlue} 0%,
-      ${theme.colors.cornflowerBlue} 50%,
-      ${theme.colors.cornflowerBlueLight} 50%,
-      ${theme.colors.cornflowerBlueLight} 100%
-    );
+    ${flexCenter}
+    position: relative;
+    width: ${MOBILE_HEIGHT}px;
+    height: 100%;
+    background: ${theme.colors.cornflowerBlue};
+    border-top-right-radius: ${theme.borderRadius[20]};
     border-bottom-right-radius: ${theme.borderRadius[20]};
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    overflow: hidden;
+
+    // Ensure Logo overlays ::after pseudo element
+    > span {
+      z-index: 1;
+    }
+
+    ::after {
+      content: '';
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      top: calc(${MOBILE_HEIGHT}px / 2);
+      background: ${theme.colors.cornflowerBlueLight};
+      border-top-left-radius: ${theme.borderRadius[16]};
+    }
+
+    @media ${devices.tablet} {
+      width: ${TABLET_HEIGHT}px;
+
+      ::after {
+        top: calc(${TABLET_HEIGHT}px / 2);
+      }
+    }
+
+    @media ${devices.laptop} {
+      width: 100%;
+      height: ${LAPTOP_WIDTH}px;
+
+      ::after {
+        top: calc(${LAPTOP_WIDTH}px / 2);
+      }
+    }
   `}
 `;
 
-export const UserContainer = styled.div`
-  height: 90px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-top: 1px solid ${({ theme }) => theme.colors.baliHai};
+export const LogoShape = styled.div`
+  position: absolute;
+  background: ${({ theme }) => theme.colors.cornflowerBlue};
 `;
 
-export const Image = styled(NextImage)`
+export const UserContainer = styled.div`
+  ${flexCenter}
+  padding: 0 24px;
+  border-left: 1px solid ${({ theme }) => theme.colors.baliHai};
+
+  @media ${devices.tablet} {
+    padding: 0 32px;
+  }
+
+  @media ${devices.laptop} {
+    height: 90px;
+    border-left: none;
+    border-top: 1px solid ${({ theme }) => theme.colors.baliHai};
+  }
+`;
+
+export const ProfileImage = styled(NextImage)`
   border-radius: 50%;
 `;
--- src/components/molecules/NavigationBar/NavigationBar.tsx
+++ src/components/molecules/NavigationBar/NavigationBar.tsx
@@ -1,17 +1,31 @@
 import Image from 'next/image';
 import { FC } from 'react';
 
+import { useMediaQuery } from '@src/hooks/useMediaQuery';
+import { devices } from '@styles/mediaQueries';
+
 import * as S from './NavigationBar.styled';
 
-const NavigationBar: FC = () => (
-  <S.Header>
-    <S.LogoContainer>
-      <Image alt="Logo" src="/logo-light.svg" width={40} height={40} />
-    </S.LogoContainer>
-    <S.UserContainer>
-      <S.Image alt="Profile" src="/image-avatar.jpg" width={40} height={40} />
-    </S.UserContainer>
-  </S.Header>
-);
+const LAPTOP_IMG_SIZE = 40;
+const TABLET_IMG_SIZE = 32;
+const MOBILE_IMG_SIZE = 28;
+
+const NavigationBar: FC = () => {
+  const isTablet = useMediaQuery(devices.tablet);
+  const isLaptop = useMediaQuery(devices.laptop);
+
+  const imgSize = isLaptop ? LAPTOP_IMG_SIZE : isTablet ? TABLET_IMG_SIZE : MOBILE_IMG_SIZE;
+
+  return (
+    <S.Header>
+      <S.LogoContainer>
+        <Image alt="Logo" src="/logo-light.svg" width={imgSize} height={imgSize} />
+      </S.LogoContainer>
+      <S.UserContainer>
+        <S.ProfileImage alt="Profile" src="/image-avatar.jpg" width={imgSize} height={imgSize} />
+      </S.UserContainer>
+    </S.Header>
+  );
+};
 
 export default NavigationBar;
