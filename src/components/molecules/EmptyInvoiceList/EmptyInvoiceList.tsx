b270217e460927cfe8257099e3eac178bb491fbd
jkellerman <89940123+jkellerman@users.noreply.github.com>
2023-01-24 11:08:17 +0000

    refactor: typography for empty invoice list

--- src/components/molecules/EmptyInvoiceList/EmptyInvoiceList.tsx
+++ src/components/molecules/EmptyInvoiceList/EmptyInvoiceList.tsx
@@ -1,22 +1,29 @@
 import Image from 'next/image';
 import { FC } from 'react';
+import { useTheme } from 'styled-components';
 
+import Typography from '@components/atoms/Typography';
 import { ETypographyVariant } from '@enums/typography';
 
 import * as S from './EmptyInvoiceList.styled';
 
 const EmptyInvoiceList: FC = () => {
+  const theme = useTheme();
+
   return (
     <S.EmptyListContainer>
       <S.IllustrationContainer>
         <Image src="/illustration-empty.svg" alt="illustration" width="241" height="200" />
       </S.IllustrationContainer>
-      <S.Heading as="h3" displayAs={ETypographyVariant.H3}>
+      <S.Heading as="h2" displayAs={ETypographyVariant.H2}>
         There is nothing here
       </S.Heading>
       <S.Paragraph as="p" displayAs={ETypographyVariant.Body}>
         Create an invoice by clicking the{' '}
-        <S.Span displayAs={ETypographyVariant.Body}>New Invoice</S.Span> button and get started
+        <Typography displayAs={ETypographyVariant.H4} $color={theme.colors.baliHai}>
+          New Invoice
+        </Typography>{' '}
+        button and get started
       </S.Paragraph>
     </S.EmptyListContainer>
   );
