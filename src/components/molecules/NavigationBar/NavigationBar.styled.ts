2fa8e771fbcf973dad5bb58dc5a2d4aef9bf3b72
Francisco Silva <fmsilva1996@gmail.com>
2023-01-25 13:47:52 +0200

    feat: add responsive styles to popup user menu

--- src/components/molecules/NavigationBar/NavigationBar.styled.ts
+++ src/components/molecules/NavigationBar/NavigationBar.styled.ts
@@ -1,36 +1,47 @@
 import NextImage from 'next/image';
 import styled, { css } from 'styled-components';
 
+import { Heading4 } from '@components/atoms/Typography';
 import { devices } from '@styles/mediaQueries';
-import { flexCenter } from '@styles/snippets';
+import { boldBoxShadow, flexCenter } from '@styles/snippets';
 
 const MOBILE_HEIGHT = 72;
 const TABLET_HEIGHT = 80;
 const LAPTOP_WIDTH = 100;
 
-export const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
+export const Wrapper = styled.div`
+  position: relative;
   width: 100%;
   height: ${MOBILE_HEIGHT}px;
-  z-index: ${({ theme }) => theme.zIndex.navbar};
-  background-color: ${({ theme }) => theme.colors.slateGray};
 
   @media ${devices.tablet} {
     height: ${TABLET_HEIGHT}px;
   }
 
   @media ${devices.laptop} {
-    height: 100vh;
-    width: ${LAPTOP_WIDTH}px;
     position: fixed;
     top: 0;
     left: 0;
+    height: 100vh;
+    width: ${LAPTOP_WIDTH}px;
+  }
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  z-index: ${({ theme }) => theme.zIndex.navbar};
+  background-color: ${({ theme }) => theme.colors.slateGray};
+
+  @media ${devices.laptop} {
+    flex-direction: column;
+    justify-content: space-between;
     border-top-right-radius: ${({ theme }) => theme.borderRadius[20]};
     border-bottom-right-radius: ${({ theme }) => theme.borderRadius[20]};
     overflow: hidden;
-    flex-direction: column;
-    justify-content: space-between;
   }
 `;
 
@@ -38,14 +49,14 @@ export const LogoContainer = styled.div`
   ${({ theme }) => css`
     ${flexCenter}
     position: relative;
+    height: ${MOBILE_HEIGHT}px;
     width: ${MOBILE_HEIGHT}px;
-    height: 100%;
     background: ${theme.colors.cornflowerBlue};
     border-top-right-radius: ${theme.borderRadius[20]};
     border-bottom-right-radius: ${theme.borderRadius[20]};
     overflow: hidden;
 
-    // Ensure Logo overlays ::after pseudo element
+    /* Ensure Logo overlays ::after pseudo element */
     > span {
       z-index: 1;
     }
@@ -55,33 +66,21 @@ export const LogoContainer = styled.div`
       position: absolute;
       height: 100%;
       width: 100%;
-      top: calc(${MOBILE_HEIGHT}px / 2);
+      transform: translateY(50%);
       background: ${theme.colors.cornflowerBlueLight};
       border-top-left-radius: ${theme.borderRadius[16]};
     }
 
     @media ${devices.tablet} {
+      height: ${TABLET_HEIGHT}px;
       width: ${TABLET_HEIGHT}px;
-
-      ::after {
-        top: calc(${TABLET_HEIGHT}px / 2);
-      }
     }
 
     @media ${devices.laptop} {
-      width: 100%;
+      width: ${LAPTOP_WIDTH}px;
       height: ${LAPTOP_WIDTH}px;
-
-      ::after {
-        top: calc(${LAPTOP_WIDTH}px / 2);
-      }
     }
   `}
-`;
-
-export const LogoShape = styled.div`
-  position: absolute;
-  background: ${({ theme }) => theme.colors.cornflowerBlue};
 `;
 
 export const UserContainer = styled.div`
@@ -94,7 +93,7 @@ export const UserContainer = styled.div`
   }
 
   @media ${devices.laptop} {
-    height: 90px;
+    padding: 32px 0;
     border-left: none;
     border-top: 1px solid ${({ theme }) => theme.colors.baliHai};
   }
@@ -105,30 +104,51 @@ export const ProfileImage = styled(NextImage)`
   cursor: pointer;
 `;
 
+const POPUP_MENU_WIDTH = 150;
+
 export const PopupMenu = styled.div<{ $isOpen: boolean }>`
-  max-width: 0;
-  background-color: ${({ theme }) => theme.colors.slateGray};
-  color: ${({ theme }) => theme.colors.whisper};
   position: absolute;
-  bottom: 2rem;
-  left: 6.5rem;
-  z-index: ${({ theme }) => theme.zIndex.modal};
+  top: 90%;
+  right: -${POPUP_MENU_WIDTH}px;
+  width: ${POPUP_MENU_WIDTH}px;
+  background-color: white;
+  transition: all 200ms ease-in-out;
   border-radius: ${({ theme }) => theme.borderRadius[8]};
-  transition: max-height 200ms ease-in-out, padding 200ms ease-in-out;
+  z-index: ${({ theme }) => theme.zIndex.modal};
+  overflow: hidden;
+  ${boldBoxShadow};
 
   ${({ $isOpen }) =>
     $isOpen &&
     css`
-      max-width: 100%;
-      padding: 1rem;
+      right: 8px;
     `}
+
+  @media ${devices.laptop} {
+    right: auto;
+    top: auto;
+    bottom: 32px;
+    left: calc(${LAPTOP_WIDTH}px - ${POPUP_MENU_WIDTH}px);
+    z-index: ${({ theme }) => theme.zIndex.dialog};
+
+    ${({ $isOpen }) =>
+      $isOpen &&
+      css`
+        right: auto;
+        left: calc(${LAPTOP_WIDTH}px + 8px);
+      `}
+  }
 `;
 
 export const LinkContent = styled.a`
+  ${Heading4}
+  display: block;
+  padding: 16px 24px;
+  color: ${({ theme }) => theme.colors.shipCove};
+  background-color: ${({ theme }) => theme.colors.selagoLight};
   cursor: pointer;
-  text-transform: capitalize;
+
   :hover {
-    color: ${({ theme }) => theme.colors.baliHai};
-    transition: 200ms;
+    background-color: ${({ theme }) => theme.colors.selago};
   }
 `;
--- src/components/molecules/NavigationBar/NavigationBar.tsx
+++ src/components/molecules/NavigationBar/NavigationBar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { FC, useState } from 'react';
 
 import { useAuth } from '@src/contexts/AuthContext';
+import { useClickOutside } from '@src/hooks/useClickOutside';
 import { useMediaQuery } from '@src/hooks/useMediaQuery';
 import { devices } from '@styles/mediaQueries';
 
@@ -14,37 +15,49 @@ const MOBILE_IMG_SIZE = 28;
 
 const NavigationBar: FC = () => {
   const { logout } = useAuth();
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const isTablet = useMediaQuery(devices.tablet);
   const isLaptop = useMediaQuery(devices.laptop);
+  const ref = useClickOutside(() => setIsMenuOpen(false));
 
   const imgSize = isLaptop ? LAPTOP_IMG_SIZE : isTablet ? TABLET_IMG_SIZE : MOBILE_IMG_SIZE;
+  let timeoutId: ReturnType<typeof setTimeout>;
 
-  const handleMouseEnter = () => setIsOpen(true);
-  const handleMouseLeave = () => setIsOpen(false);
+  const toggleMenu = () => setIsMenuOpen((prevState) => !prevState);
+  const closeMenuWithDelay = (delay: number) => {
+    timeoutId = setTimeout(() => setIsMenuOpen(false), delay);
+  };
 
   return (
-    <>
+    <S.Wrapper>
       <S.Header>
         <S.LogoContainer>
           <Image alt="Logo" src="/logo-light.svg" width={imgSize} height={imgSize} />
         </S.LogoContainer>
-        <S.UserContainer>
+        <S.UserContainer
+          ref={ref}
+          onMouseEnter={() => setIsMenuOpen(true)}
+          onMouseLeave={() => closeMenuWithDelay(500)}
+        >
           <S.ProfileImage
             alt="Profile"
             src="/image-avatar.jpg"
             width={imgSize}
             height={imgSize}
-            onMouseEnter={handleMouseEnter}
+            onClick={toggleMenu}
           />
         </S.UserContainer>
       </S.Header>
-      <S.PopupMenu $isOpen={isOpen} onMouseLeave={handleMouseLeave}>
+      <S.PopupMenu
+        $isOpen={isMenuOpen}
+        onMouseEnter={() => clearTimeout(timeoutId)}
+        onMouseLeave={() => closeMenuWithDelay(300)}
+      >
         <Link href="/login">
-          <S.LinkContent onClick={() => logout()}>logout</S.LinkContent>
+          <S.LinkContent onClick={logout}>Logout</S.LinkContent>
         </Link>
       </S.PopupMenu>
-    </>
+    </S.Wrapper>
   );
 };
 export default NavigationBar;
