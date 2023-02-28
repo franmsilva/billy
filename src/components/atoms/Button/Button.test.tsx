785c589cee477ba90cdd50969204bc5e0b09c4f5
jkellerman <89940123+jkellerman@users.noreply.github.com>
2023-02-28 12:35:46 +0000

    test: add unit test for Button component

--- src/components/atoms/Button/Button.test.tsx
+++ src/components/atoms/Button/Button.test.tsx
@@ -1,13 +1,22 @@
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { renderWithTheme } from '@src/utils/tests';
 
 import Button from './Button';
 
 describe('<Button />', () => {
-  beforeEach(() => renderWithTheme(<Button>Click me!</Button>));
+  const onClick = jest.fn();
+  beforeEach(() => renderWithTheme(<Button onClick={onClick}>Click me!</Button>));
 
   it('renders without error', () => {
     expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('calls onClick when clicked', () => {
+    act(() => {
+      userEvent.click(screen.getByRole('button'));
+    });
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
